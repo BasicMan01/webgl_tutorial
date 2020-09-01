15848467bf6a43146037dd4ed3d7c7456b0f3cdd
BasicMan01 <Basic33833@gmail.com>
2020-09-01 18:30:29 +0200

    Refactoring 360 degree video example

--- tutorial/threejs/vr/video360/js/main.js
+++ tutorial/threejs/vr/video360/js/main.js
@@ -20,16 +20,23 @@ import { VRButton } from '../../../../../lib/threejs_119/examples/jsm/webxr/VRBu
 		this.canvas = canvas;
 
 		this.camera = null;
-		this.cameraGroup = null;
 		this.clock = null;
 		this.controls = null;
 		this.renderer = null;
 		this.scene = null;
 
-		this.meshL = null;
-		this.meshR = null;
+		this.sphere = null;
 		this.video = null;
 		this.videoTexture = null;
+
+		this.isMouseDown = false;
+		this.lon = 0;
+		this.lat = 0;
+		this.phi = 0;
+		this.theta = 0;
+		this.mouseDownLon = 0;
+		this.mouseDownLat = 0;
+		this.mouseMoveStart = new THREE.Vector2();
 	};
 
 	Main.prototype.init = function() {
@@ -37,12 +44,8 @@ import { VRButton } from '../../../../../lib/threejs_119/examples/jsm/webxr/VRBu
 		this.scene = new THREE.Scene();
 
 		this.camera = new THREE.PerspectiveCamera(config.CAMERA_FOV, this.getCameraAspect(), config.CAMERA_NEAR_PLANE, config.CAMERA_FAR_PLANE);
-		this.camera.layers.enable(1);
-
-		this.cameraGroup = new THREE.Group();
-		this.cameraGroup.add(this.camera);
-
-		this.scene.add(this.cameraGroup);
+		this.camera.target = new THREE.Vector3(0, 0, 0);
+		//this.camera.layers.enable(1);
 
 		this.renderer = new THREE.WebGLRenderer({antialias: true});
 		this.renderer.setClearColor(0x000000, 1);
@@ -51,33 +54,20 @@ import { VRButton } from '../../../../../lib/threejs_119/examples/jsm/webxr/VRBu
 		this.renderer.xr.enabled = true;
 		this.renderer.xr.setReferenceSpaceType('local');
 
-		this.controls = new OrbitControls(this.cameraGroup, this.renderer.domElement);
-		this.controls.rotateSpeed = 0.3;
-		this.controls.enableZoom = true;
-		this.controls.enablePan = true;
-
-		this.cameraGroup.rotation.y = Math.PI / 2
-
-		this.controls.target.set(
-			this.cameraGroup.position.x + 0.001,
-			this.cameraGroup.position.y,
-			this.cameraGroup.position.z
-
-		);
-
-
-
 		// add renderer to the DOM-Tree
 		this.canvas.appendChild(this.renderer.domElement);
 		this.canvas.appendChild(VRButton.createButton(this.renderer));
 
 		this.onDeviceOrientationHandler = this.onDeviceOrientationHandler.bind(this);
 
-		this.renderer.domElement.addEventListener('click', this.onClickHandler.bind(this));
+		this.renderer.domElement.addEventListener('mousedown', this.onMouseDown.bind(this), false);
+		this.renderer.domElement.addEventListener('mousemove', this.onMouseMove.bind(this), false);
+		this.renderer.domElement.addEventListener('mouseup', this.onMouseUp.bind(this), false);
 
 		window.addEventListener('deviceorientation', this.onDeviceOrientationHandler, false);
 		window.addEventListener('resize', this.onResizeHandler.bind(this), false);
 		window.addEventListener('vrdisplaypresentchange', function (event) {
+			// Maintain viewing direction when switching to VR and back
 			if(event.display.isPresenting){
 				this.controls.enabled = false;
 			}
@@ -89,35 +79,24 @@ import { VRButton } from '../../../../../lib/threejs_119/examples/jsm/webxr/VRBu
 	};
 
 	Main.prototype.createObject = function() {
-		let self = this;
-
 		this.video = document.createElement('video');
 		this.video.crossOrigin = 'anonymous';
 		this.video.loop = true;
 		this.video.muted = true;
 		this.video.src = this.canvas.dataset.src;
-		this.video.setAttribute( 'webkit-playsinline', 'webkit-playsinline' );
+		this.video.setAttribute('webkit-playsinline', 'webkit-playsinline');
 
 		this.videoTexture = new THREE.VideoTexture(this.video);
-		this.videoTexture.minFilter = THREE.NearestFilter;
-		this.videoTexture.maxFilter = THREE.NearestFilter;
+		this.videoTexture.minFilter = THREE.LinearFilter;
+		this.videoTexture.maxFilter = THREE.LinearFilter;
 		this.videoTexture.format = THREE.RGBFormat;
 
-		this.meshL = new THREE.Mesh(
+		this.sphere = new THREE.Mesh(
 			this.getGeometry(),
 			new THREE.MeshBasicMaterial({ map: this.videoTexture })
 		);
 
-		this.meshR = new THREE.Mesh(
-			this.getGeometry(),
-			new THREE.MeshBasicMaterial({ map: this.videoTexture })
-		);
-
-		this.meshL.layers.set(1);
-		this.meshR.layers.set(2);
-
-		this.scene.add(this.meshL);
-		this.scene.add(this.meshR);
+		this.scene.add(this.sphere);
 	};
 
 	Main.prototype.getGeometry = function() {
@@ -134,7 +113,19 @@ import { VRButton } from '../../../../../lib/threejs_119/examples/jsm/webxr/VRBu
 			this.videoTexture.update();
 		}
 
-		this.controls.update(this.clock.getDelta());
+		if (this.controls) {
+			this.controls.update(this.clock.getDelta());
+		} else {
+			this.lat = Math.max(-85, Math.min(85, this.lat));
+			this.phi = THREE.MathUtils.degToRad(90 - this.lat);
+			this.theta = THREE.MathUtils.degToRad(this.lon);
+
+			this.camera.position.x = 0.001 * Math.sin(this.phi) * Math.cos(this.theta);
+			this.camera.position.y = 0.001 * Math.cos(this.phi);
+			this.camera.position.z = 0.001 * Math.sin(this.phi) * Math.sin(this.theta);
+
+			this.camera.lookAt(this.camera.target);
+		}
 
 		this.renderer.render(this.scene, this.camera);
 	};
@@ -144,23 +135,37 @@ import { VRButton } from '../../../../../lib/threejs_119/examples/jsm/webxr/VRBu
 
 	Main.prototype.getCameraAspect = function() { return this.getCanvasWidth() / this.getCanvasHeight(); };
 
-	Main.prototype.onClickHandler = function(event) {
-		this.video.play();
-	};
-
 	Main.prototype.onDeviceOrientationHandler = function(event) {
 		if(!event.alpha) {
 			return;
 		}
 
-		// Create controls for mobile.
-		this.controls.enabled = false;
-
-		this.controls = new DeviceOrientationControls(this.cameraGroup);
+		this.controls = new DeviceOrientationControls(this.camera);
 		this.controls.connect();
 		this.controls.update();
 
 		window.removeEventListener('deviceorientation', this.onDeviceOrientationHandler, false);
+	};
+
+	Main.prototype.onMouseDown = function(event) {
+		event.preventDefault();
+
+		this.isMouseDown = true;
+		this.mouseMoveStart.set(event.clientX, event.clientY);
+
+		this.mouseDownLon = this.lon;
+		this.mouseDownLat = this.lat;
+	};
+
+	Main.prototype.onMouseMove = function(event) {
+		if (this.isMouseDown) {
+			this.lon = (this.mouseMoveStart.x - event.clientX) * 0.1 + this.mouseDownLon;
+			this.lat = (this.mouseMoveStart.y - event.clientY) * 0.1 + this.mouseDownLat;
+		}
+	};
+
+	Main.prototype.onMouseUp = function(event) {
+		this.isMouseDown = false;
 	};
 
 	Main.prototype.onResizeHandler = function(event) {
@@ -174,6 +179,15 @@ import { VRButton } from '../../../../../lib/threejs_119/examples/jsm/webxr/VRBu
 
 	let main = new Main(document.getElementById('webGlCanvas'));
 	document.addEventListener('DOMContentLoaded', function() {
+		document.getElementById('btnPlay').style.border = 'none';
+		document.getElementsByClassName('button-container')[0].style.top = (main.getCanvasHeight() / 3) + 'px';
+
+		document.getElementById('btnPlay').addEventListener('click', function() {
+			this.style.display = 'none';
+
+			main.video.play();
+		});
+
 		main.init();
 	});
 }(window));
